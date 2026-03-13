2a5fdda8f5e82a6cc56468f7518553db3b436226
Kamal Swarnkar <swarnkar889@gmail.com>
2026-03-13 19:14:38 +0530

    final deployment phase

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qb$-(s8nu@$doo*@@joj-jl9_=mznc3sqofe-dsucu)03)p!8&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
