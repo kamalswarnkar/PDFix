06ac28e619c552e96bc8ba8afe5771ea9a0c6971
Kamal Swarnkar <swarnkar889@gmail.com>
2026-03-21 16:14:44 +0530

    cleanup fix for deployment

--- tools/utils/cleanup.py
+++ tools/utils/cleanup.py
@@ -2,9 +2,10 @@ import os
 import time
 from django.conf import settings
 
+
 def cleanup_old_files():
-    #print("chal raha hai")
     folder = settings.MEDIA_ROOT
+    os.makedirs(folder, exist_ok=True)
 
     now = time.time()
 
@@ -12,12 +13,16 @@ def cleanup_old_files():
         file_path = os.path.join(folder, filename)
 
         if os.path.isfile(file_path):
-            file_age = now - os.path.getmtime(file_path)
+            try:
+                file_age = now - os.path.getmtime(file_path)
+            except OSError:
+                # File may be deleted or moved while iterating.
+                continue
 
-            #30 mins = 1800 sec
+            # 30 mins = 1800 sec
             if file_age > 1800:
                 try:
                     os.remove(file_path)
-                    print("file deleted")
-                except PermissionError:
-                    print("file still in use: ", filename)
+                except (PermissionError, OSError):
+                    # Ignore files currently in use or concurrently modified.
+                    pass
